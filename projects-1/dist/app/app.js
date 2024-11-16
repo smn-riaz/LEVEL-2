b38b13155a2c4b3c7ee13e51f76c904e49db6507
shahman-riaz <shahmanriaz07@gmail.com>
2024-11-16 19:17:40 +0600

    updated

--- projects-1/dist/app/app.js
+++ projects-1/dist/app/app.js
@@ -9,12 +9,44 @@ const port = 3000;
 //parser
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use('/', userRouter);
+app.use('/api/v1/course', courseRouter);
+userRouter.post('/api/v1/users/create-user', (req, res) => {
+    const user = req.body;
+    // console.log(user);
+    res.json({
+        success: true,
+        message: "User is created Successfully",
+        data: user
+    });
+});
+courseRouter.post('/create-course', (req, res) => {
+    const course = req.body;
+    res.json({
+        success: true,
+        message: "User is created Successfully",
+        data: course
+    });
+});
 const logger = (req, res, next) => {
     // console.log(req.url, req.method, req.hostname)
     next();
 };
-app.get('/', logger, (req, res) => {
-    res.send('Hello World from Shahman Riaz');
+app.get('/', logger, (req, res, next) => {
+    try {
+        res.send(something);
+    }
+    catch (error) {
+        console.log(error);
+        next(error);
+        // If error is not handled globally
+        // res.status(400).json({
+        //   success: false,
+        //   message:"Failed to get data"
+        // })
+    }
 });
 app.post('/', (req, res) => {
     // console.log(req.body);
@@ -32,5 +64,21 @@ app.get("/:userId/:subId", (req, res) => {
 app.get("/", (req, res) => {
     console.log(req.query);
     res.json();
+});
+app.all('*', (req, res) => {
+    res.status(400).json({
+        success: false,
+        message: ""
+    });
+});
+//global error handler
+app.use((error, req, res, next) => {
+    console.log(error);
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: "Something went wrong"
+        });
+    }
 });
 exports.default = app;
--- projects-1/src/app/app.ts
+++ projects-1/src/app/app.ts
@@ -7,9 +7,31 @@ const port = 3000
 app.use(express.json())
 app.use(express.text())
 
+const userRouter = express.Router()
+const courseRouter = express.Router()
+
+app.use('/',userRouter)
+app.use('/api/v1/course',courseRouter)
 
 
+userRouter.post('/api/v1/users/create-user',(req:Request, res:Response)=> {
+  const user = req.body;
+  // console.log(user);
+  res.json({
+    success:true,
+    message:"User is created Successfully",
+    data:user
+  })
+})
 
+courseRouter.post('/create-course', (req:Request, res:Response) => {
+  const course = req.body
+  res.json({
+    success:true,
+    message:"User is created Successfully",
+    data:course
+  })
+})
 
 
 
@@ -28,8 +50,18 @@ const logger = (req:Request, res:Response, next:NextFunction) => {
 
 
 
-app.get('/',logger, (req:Request , res:Response) => {
-  res.send('Hello World from Shahman Riaz')
+app.get('/',logger, (req:Request , res:Response,next:NextFunction) => {
+  try{
+    res.send(something)
+  } catch(error){
+    console.log(error);
+    next(error)
+    // If error is not handled globally
+    // res.status(400).json({
+    //   success: false,
+    //   message:"Failed to get data"
+    // })
+  }
 })
 
 app.post('/', (req:Request, res:Response) => {
@@ -54,7 +86,27 @@ app.get("/",(req:Request, res:Response)=> {
 })
 
 
+app.all('*', (req:Request,res:Response) => {
+res.status(400).json({
+  success:false,
+  message:""
+})
+})
 
+
+
+
+
+//global error handler
+app.use((error:any,req:Request,res:Response,next:NextFunction)=> {
+  console.log(error);
+  if(error) {
+   res.status(400).json({
+    success:false,
+    message:"Something went wrong"
+   })
+  }
+})
 
 
 export default app
